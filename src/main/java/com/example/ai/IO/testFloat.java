e86f74eea1c19c0d7c0b143cd35f59ceebc03f21
LPF <1527831187@qq.com>
2019-08-28 12:21:44 +0800

    testfloat

---
+++ src/main/java/com/example/ai/IO/testFloat.java
@@ -0,0 +1,22 @@
+package com.example.ai.IO;
+
+public class testFloat {
+    public static void main(String[] args) {
+        //float f=1.0;//（精度高向精度低（这个需要强转））
+        float f3=1;//整形可以默认转换为float或double(精度低向精度高)
+        float f1=1.0f;
+        double d=1.0;
+        double d1=1.0d;
+        double d3=10.03d;
+
+        String s=new String();
+        System.out.println("s="+s);
+        String sa;
+        //System.out.println(sa);
+        //System.out.println(f);
+        System.out.println(f1);
+        System.out.println(d);
+        System.out.println(d3);
+
+    }
+}
