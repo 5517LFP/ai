c3e4ddbeecbd5a550712e96cfba735bf7b692e67
LPF <1527831187@qq.com>
2019-09-11 11:31:52 +0800

    蚂蚁牙黑14

--- src/main/java/com/example/ai/ai_testtwo/Gittesttwo_1.java
+++ src/main/java/com/example/ai/ai_testtwo/Gittesttwo_1.java
@@ -3,5 +3,6 @@ package com.example.ai.ai_testtwo;
 public class Gittesttwo_1 {
     public static void main(String[] args) {
         System.out.println("哎呦喂");
+        System.out.println("蚂蚁牙黑");
     }
 }
